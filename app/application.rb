cbed7b81d3e58f99e9a81fe9d08f2b19c3902893
Micah Levine <micah.levine@outlook.com>
2019-12-29 16:45:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items\/.+/")
+    if req.path.match(/items\/.+/)
       item_name = req.path.split("/items/").last
       item = @@items.detect { |i| i.name == item_name }
       if item
